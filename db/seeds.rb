4e9551cc1f28f429374b573a14aa2dda7d44275e
Roman van Eldijk <romanvaneldijk@gmail.com>
2021-11-11 17:13:45 +0100

    made mvc for bookmarks and list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "cleaning database"
+Movie.destroy_all
+
+10.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: 'https://source.unsplash.com/random',
+    rating: rand(1.1..10.0).round(1)
+  )
+  puts "new movie"
+end
+
+puts "created a lot of movies"
